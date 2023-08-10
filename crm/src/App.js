b081ba3c109444a979275779a93a2df205560f51
legityif <yifanyang80@gmail.com>
2023-08-10 12:35:55 +1000

    user login basic page

---
+++ crm/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (username === 'user' && password === 'password') {
+      alert('Login successful!');
+    } else {
+      alert('Login failed. Please check your credentials.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1 className="app-title"> CRM Application</h1>
+      <div className="login-form">
+        <h2 className="login-header">User Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
