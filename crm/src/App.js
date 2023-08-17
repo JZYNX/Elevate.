f9c30b4c397eba7b6ef333912d04436e1b5a9f27
JZYNX <91903716+JZYNX@users.noreply.github.com>
2023-08-17 12:33:13 +1000

    Added routing. Restructuring code base. Added directories.

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,39 +1,13 @@
-import React, { useState } from 'react';
+import React from "react";
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from "./pages/Login"
 
-function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = () => {
-    if (username === 'user' && password === 'password') {
-      alert('Login successful!');
-    } else {
-      alert('Login failed. Please check your credentials.');
-    }
-  };
-
-  return (
-    <div className="login-container">
-      <h1 className="app-title">CRM Application</h1>
-      <div className="login-form">
-        <h2 className="login-header">User Login</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleLogin}>Login</button>
-      </div>
-    </div>
-  );
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path = "/login" element = {<Login />} />
+      {/* <Route path = "/" element = {<Home />} /> */}
+    </Routes>
+  </BrowserRouter>
 }
-
-export default App;
