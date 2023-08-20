f9dcecf23872fa22944cbc0ced2cae10b0f0e03b
legityif <yifanyang80@gmail.com>
2023-08-20 21:56:05 +1000

    styled login page with dual sided design

--- crm/src/pages/Login.jsx
+++ crm/src/pages/Login.jsx
@@ -28,8 +28,113 @@ const BackgroundImage = styled.img`
 `;
 
 const WelcomeMessage = styled.div`
-  flex: 1.5;
-  padding: 20px;
+  flex: 1.4;
+  height: 90vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 15vh;
+  padding-left: 15vh;
+  padding-right: 15vh;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+
+  h1.app-title {
+    font-size: 36px;
+    font-weight: Normal;
+    letter-spacing: 0.05em;
+  }
+
+  h2.app-title {
+    font-size: 28px;
+    font-weight: Normal;
+    letter-spacing: 0.02em;
+  }
+`;
+
+const LoginForm = styled.div`
+  flex: 1;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 1);
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  color: #333;
+  margin-right: 15vh;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+
+  h2.login-header {
+    font-size: 26px;
+    margin-bottom: 30px;
+    font-weight: Normal;
+    letter-spacing: 0.06em;
+  }
+
+  // INPUT BOX
+  input {
+    width: 65%;
+    padding: 10px;
+    margin: 10px 0;
+    border: 2px solid #ddd;
+    border-radius: 4px;
+    font-size: 16px;
+    outline: none;
+  }
+
+  // LOGIN AND FORGET LOGIN BUTTONS
+  button.login-button {
+    width: 68%;
+    padding: 10px;
+    margin: 10px 0;
+    background-color: #0a1172;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #151e3d;
+    }
+  }
+  button.forget-button {
+    color: #0a1172;
+    background-color: white;
+    border: none;
+    cursor: pointer;
+    transition: color 0.3s;
+    padding: 10px;
+
+    &:hover {
+      color: #0056b3;
+    }
+  }
+
+  // SEPARATOR BETWEEN LOGIN AND ALT LOGINS
+  .separator-container {
+    display: flex;
+    margin: 20px 0; 
+    width: 67%;
+    padding: 10px;
+    justify-content: center;
+  }
+  .separator-line {
+    flex-grow: 1;
+    height: 0.5px;
+    background-color: black;
+  }
+  .separator-text {
+    margin: 0 10px;
+    font-size: 14px;
+    white-space: nowrap;
+  }
+
 `;
 
 const RegisterContainer = styled.div`
@@ -51,76 +156,17 @@ const RegisterContainer = styled.div`
   }
 `;
 
-const LoginForm = styled.div`
-  flex: 1;
-  height: 90vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-radius: 12px;
-  background-color: #ffffff;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-  color: #333;
-  margin-right: 10vh;
-
-  h2.login-header {
-    font-size: 28px;
-    margin-bottom: 30px;
-  }
-
-  input {
-    width: 65%;
-    padding: 10px;
-    margin: 10px 0;
-    border: 2px solid #ddd;
-    border-radius: 4px;
-    font-size: 16px;
-    outline: none;
-  }
-
-  button.login-button {
-    width: 68%;
-    padding: 10px;
-    margin: 10px 0;
-    background-color: #0a1172;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    font-size: 16px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-
-    &:hover {
-      background-color: #151e3d;
-    }
-  }
-
-  button.forget-button {
-    color: #0a1172;
-    background-color: white;
-    border: none;
-    cursor: pointer;
-    transition: color 0.3s;
-
-    &:hover {
-      color: #0056b3;
-    }
-  }
-`;
-
+// ALTERNATIVE LOGIN BUTON STYLING
 const IconOnlyButton = styled.button`
   background: none;
   border: none;
   padding: 0;
   cursor: pointer;
 `;
-
 const Icon = styled.img`
   width: 32px; /* Adjust the width and height as needed */
   height: 32px;
 `;
-
 const OtherLoginOptions = styled.div`
   display: flex;
   align-items: center;
@@ -157,10 +203,14 @@ function Login() {
     <LoginContainer>
       <BackgroundImage src={bgImg} alt="bgImg" />
       <WelcomeMessage>
-        <h1 className="app-title">Welcome</h1>
+        <h1 className="app-title">Welcome to the <strong>ELEVATE</strong> project.</h1>
+        <h2 className='app-title'><strong>ELEVATE</strong> your life </h2>
+        <h2 className='app-title'><strong>ELEVATE</strong> your skill </h2>
+        <h2 className='app-title'><strong>ELEVATE</strong> your skins </h2>
+        <h2 className='app-title'><strong>ELEVATE</strong> and reach Radiant </h2>
       </WelcomeMessage>
       <LoginForm>
-        <h2 className="login-header">LOG IN TO VALORANT</h2>
+        <h2 className="login-header"> Welcome to <strong>ELEVATE</strong></h2>
         <RegisterContainer>
           Don't have an account yet? 
           <button className="register-button" onClick={() => handleNavigation('/register')}>
@@ -188,6 +238,11 @@ function Login() {
         <button className="login-button" onClick={handleLogin}>
           Login
         </button>
+        <div className="separator-container">
+          <hr className="separator-line" />
+          <div className="separator-text">OR</div>
+          <hr className="separator-line" />
+        </div>
         <OtherLoginOptions>
           <IconOnlyButton>
             <Icon src={googleIcon} alt="Google" />
