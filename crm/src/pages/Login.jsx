92f40dd864ecad08f1474f3f65efc088e1ab310a
legityif <yifanyang80@gmail.com>
2023-08-20 15:59:41 +1000

    testing background image

--- crm/src/pages/Login.jsx
+++ crm/src/pages/Login.jsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-
 const LoginContainer = styled.div`
   display: flex;
   height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  background-color: #BB4BD5;
+  // background-color: #bb4bd5;
+  background-image: url("../blue.jpg");
   color: white;
 `;
 
@@ -20,18 +21,18 @@ const GraphicSection = styled.div`
 const RegisterContainer = styled.div`
   font-size: 16px;
   color: #888;
-  
-  button.register-button {  
+
+  button.register-button {
     cursor: pointer;
-    color: #BB4BD5;
+    color: #bb4bd5;
     background-color: white;
     border: none;
     font-size: 16px;
-  }
+    transition: color 0.3s;
 
-  button.register-button:hover {
-    color: #0056b3; 
-    cursor: pointer;
+    &:hover {
+      color: #0056b3;
+    }
   }
 `;
 
@@ -43,21 +44,21 @@ const LoginForm = styled.div`
   justify-content: center;
   align-items: center;
   border-radius: 12px;
-  background-color: #ffffff; 
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); 
-  color: #333; 
+  background-color: #ffffff;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  color: #333;
   margin-right: 10vh;
 
   h2.login-header {
     font-size: 24px;
     margin-bottom: 20px;
   }
-  
+
   input {
     width: 65%;
     padding: 10px;
     margin: 10px 0;
-    border: 2px solid #ddd; 
+    border: 2px solid #ddd;
     border-radius: 4px;
     font-size: 16px;
     outline: none;
@@ -67,57 +68,74 @@ const LoginForm = styled.div`
     width: 50%;
     padding: 10px;
     margin: 10px 0;
-    background-color: #BB4BD5; 
-    color: white; 
+    background-color: #bb4bd5;
+    color: white;
     border: none;
     border-radius: 4px;
     font-size: 16px;
     cursor: pointer;
-    transition: background-color 0.3s; 
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #a231bb;
+    }
   }
 
-  button.login-button:hover {
-    background-color: #A231BB; 
-  }
-
-  button.forget-button{
-    color: #BB4BD5;
+  button.forget-button {
+    color: #bb4bd5;
     background-color: white;
     border: none;
-  }
-
-  button.forget-button:hover {
-    color: #0056b3; 
     cursor: pointer;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #0056b3;
+    }
   }
-`
+`;
 
 const LineBreak = styled.div`
   width: 100%;
-  height: 20px; 
-  border-bottom: 1px solid black; 
-  text-align: center">
+  text-align: center;
+  margin-bottom: 20px;
 
   span {
-    font-size: 40px; 
-    background-color: #F3F5F6; 
+    font-size: 18px;
+    background-color: #f3f5f6;
     padding: 0 10px;
-  } 
+  }
 `;
 
 const OtherLoginOptions = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  button {
+    margin: 5px;
+    padding: 10px;
+    background-color: #bb4bd5;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #a231bb;
+    }
+  }
 `;
 
+
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [credentials, setCredentials] = useState({ username: '', password: '' });
 
   const navigate = useNavigate();
 
   const handleLogin = () => {
+    const { username, password } = credentials;
     if (username === 'user' && password === 'password') {
       navigate('/dashboard');
     } else {
@@ -131,14 +149,9 @@ function Login() {
     }
   };
 
-  const handleForgotPassword = () => {
-    navigate('/dashboard')
+  const handleNavigation = (path) => {
+    navigate(path);
   };
-
-  const handleRegister = () => {
-    navigate('/dashboard')
-  };
-
 
   return (
     <LoginContainer>
@@ -149,43 +162,43 @@ function Login() {
         <h2 className="login-header">User Login</h2>
         <RegisterContainer>
           Don't have an account yet? 
-          <button className="register-button" onClick={handleRegister}>
-              Register
+          <button className="register-button" onClick={() => handleNavigation('/dashboard')}>
+            Register
           </button>
         </RegisterContainer>
         <input
           className="user-input"
           type="text"
           placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={credentials.username}
+          onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
         />
-        <button className="forget-button" onClick={handleForgotPassword}>
+        <button className="forget-button" onClick={() => handleNavigation('/dashboard')}>
           Forgot your password?
         </button>
         <input
-          className='password-input'
+          className="password-input"
           placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={credentials.password}
+          onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
           onKeyPress={handleKeyPress}
         />
         <button className="login-button" onClick={handleLogin}>
           Login
         </button>
-
-      <OtherLoginOptions>
-        <LineBreak>
-          <span>or login with</span>
-        </LineBreak>
-        <div>
-          <button>Login with Google</button>
-          <button>Login with Apple ID</button>
-        </div>
-      </OtherLoginOptions>
+        <OtherLoginOptions>
+          <LineBreak>
+            <span>or login with</span>
+          </LineBreak>
+          <div>
+            <button>Login with Google</button>
+            <button>Login with Apple ID</button>
+          </div>
+        </OtherLoginOptions>
       </LoginForm>
     </LoginContainer>
   );
 }
 
 export default Login;
+
