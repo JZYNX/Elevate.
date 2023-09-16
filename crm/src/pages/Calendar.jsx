4c66996c11c3a378b62845d5f6ef7cc9e963d52f
jz <jasontang02@outlook.com>
2023-09-16 18:24:15 +1000

    calendar popup
    calendar page has popup to add event
    incomplete!! - requires proper date time selection

--- crm/src/pages/Calendar.jsx
+++ crm/src/pages/Calendar.jsx
@@ -6,7 +6,11 @@ import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker'; 
 import "../styles/calendar.css";
+
+Modal.setAppElement('#root');
 
 const CalendarContainer = styled.div`
     display: flex;
@@ -23,24 +27,53 @@ const SidebarColumn = styled.div`
 
 const FullCalendarContainer = styled.div`
     position: relative;
-    left: 5rem;
-    width: 70%;
+    margin-top: 1rem;
+    padding: 2rem;
+    width: 100%;
     color: black;
-    padding-top: 40px;
 `;
 
 function Calendar() {
-  const [currentEvents, setCurrentEvents] = useState([]);
-  const handleDateClick = (info) => {
-    const title = prompt('Please enter a title for the event:');
-    if (title) {
-      const calendarApi = info.view.calendar;
-      calendarApi.addEvent({
-        title,
-        start: info.dateStr, // Use the clicked date as the event start
-        allDay: !info.allDay, // All-day or not
-      });
-    }
+  const [currEvents, setEvents] = useState([
+    {
+      title: 'Event 1',
+      start: '2023-09-20T10:00:00',
+      end: '2023-09-20T12:00:00',
+    },
+    {
+      title: 'Event 2',
+      start: '2023-09-21T14:00:00',
+      end: '2023-09-21T16:00:00',
+    },
+  ]);
+  const [eventName, setEventName] = useState('');
+  const [startDatetime, setStartDatetime] = useState(new Date());
+  const [endDatetime, setEndDatetime] = useState(new Date());
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleDateClick = () => {
+    setIsModalOpen(true);
+    document.querySelector('.fc').style.selectable = 'false';
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  }
+
+  const handleCreateEvent = () => {
+    const newEvent = {
+      title: eventName,
+      start: startDatetime.toISOString(),
+      end: endDatetime.toISOString(),
+    };
+  
+    setEvents([...currEvents, newEvent]);
+  
+    setEventName('');
+    setStartDatetime(new Date());
+    setEndDatetime(new Date());
+  
+    setIsModalOpen(false);
   };
 
   return (
@@ -55,16 +88,47 @@ function Calendar() {
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
             initialView="dayGridMonth" 
             headerToolbar={{
-              left: "prev today next",
+              left: "prev,today,next",
               center: "title",
               right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
             }}
-            events={currentEvents} 
+            events={currEvents} 
             dateClick={handleDateClick} 
             selectable={true}
             editable={true}
             selectMirror={true}
         />
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={handleModalClose}
+          contentLabel="Event Details"
+          className="modal-content"
+          overlayClassName="modal-overlay"
+        >
+          <h2 className='event-title'>Create Event</h2>
+          <input
+            type="text"
+            placeholder="Event Name"
+            value={eventName}
+            onChange={(e) => setEventName(e.target.value)}
+          />
+          <div className="datetime-picker">
+            <label>Start Date and Time:</label>
+            <DateTimePicker
+              onChange={(date) => setStartDatetime(date)}
+              value={startDatetime}
+            />
+          </div>
+          <div className="datetime-picker">
+            <label>End Date and Time:</label>
+            <DateTimePicker
+              onChange={(date) => setEndDatetime(date)}
+              value={endDatetime}
+            />
+          </div>
+          <button onClick={handleCreateEvent}>Create Event</button>
+          <button onClick={handleModalClose}>Cancel</button>
+        </Modal>
       </FullCalendarContainer>
     </CalendarContainer>
   );
