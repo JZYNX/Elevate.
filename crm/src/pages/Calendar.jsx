d2295be0d36a1305c06d1fe58276cc2b10fcd0ea
jz <jasontang02@outlook.com>
2023-09-16 13:06:31 +1000

    Styling
    removed scroll wheels for day and all-day
    changed border color for active buttons

--- crm/src/pages/Calendar.jsx
+++ crm/src/pages/Calendar.jsx
@@ -1,4 +1,4 @@
-import {React, useState}from 'react';
+import {React, useState, useEffect} from 'react';
 import styled from 'styled-components';
 import Sidebar from '../components/Sidebar';
 import FullCalendar from "@fullcalendar/react";
