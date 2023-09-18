62416427d48032cc2b216810efdbb6fb7e8f1621
legityif <yifanyang80@gmail.com>
2023-09-18 19:39:13 +1000

    added stats row to dashboard
    data for stats row

--- crm/src/pages/Dashboard.jsx
+++ crm/src/pages/Dashboard.jsx
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 import Sidebar from '../components/Sidebar';
 import bgImg from '../assets/nikuubg.jpg';
 
+const BackgroundImage = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: right;
+  z-index: -1;
+`;
+
 const DashboardContainer = styled.div`
   display: flex;
   height: 100vh;
@@ -15,54 +24,56 @@ const SidebarColumn = styled.div`
   background-color: #f0f0f0;
 `;
 
-const BackgroundImage = styled.img`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: right;
-  z-index: -1;
+// stats row on dashboard
+const StatsContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  margin-left: 10%;
+`;
+const StatBox = styled.div`
+  flex: 1;
+  text-align: left;
+`;
+const Number = styled.h2`
+  font-size: 40px; /* Adjust the font size as needed */
+  padding-top: 30px;
+  margin-bottom: 0;
+  font-weight: bold;
+`;
+const Descriptor = styled.p`
+  font-size: 20px; /* Adjust the font size as needed */
+  margin-top: 5px;
 `;
 
+const EventsContainer = styled.div`
+
+`;
+
+
 function Dashboard() {
-  // Example data for tasks and notifications
-  const todayTasks = [
-    { id: 1, title: 'Complete project report', time: '10:00 AM' },
-    { id: 2, title: 'Meeting with client', time: '02:00 PM' },
-    // Add more tasks
-  ];
-
-  const recentNotifications = [
-    { id: 1, text: 'New email from John Doe', time: '2 minutes ago' },
-    { id: 2, text: 'Upcoming event reminder', time: '1 hour ago' },
-    // Add more notifications
-  ];
-
   return (
     <DashboardContainer>
       <SidebarColumn>
         <Sidebar />
       </SidebarColumn>
       <BackgroundImage src={bgImg} alt="bgImg" />
-      <div className="dashboard"> 
-        <h2>Dashboard</h2>
-        <div className="task-list"> 
-          <h3>Today's Tasks</h3>
-          <ul>
-            {todayTasks.map(task => (
-              <li key={task.id} className="task">{task.title} - {task.time}</li> 
-            ))}
-          </ul>
-        </div>
-        <div className="notification-list"> 
-          <h3>Recent Notifications</h3>
-          <ul>
-            {recentNotifications.map(notification => (
-              <li key={notification.id} className="notification">{notification.text} - {notification.time}</li> 
-            ))}
-          </ul>
-        </div>
-      </div>
+      <StatsContainer>
+        <StatBox>
+          <Number>150</Number>
+          <Descriptor>Connections</Descriptor>
+        </StatBox>
+        <StatBox>
+          <Number>12</Number>
+          <Descriptor>Upcoming Events</Descriptor>
+        </StatBox>
+        <StatBox>
+          <Number>4</Number>
+          <Descriptor>To-do Tasks</Descriptor>
+        </StatBox>
+      </StatsContainer>
+
+
     </DashboardContainer>
   );
 }
