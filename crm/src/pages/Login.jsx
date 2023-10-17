ab5380d055341020d53338abf436349c67e4f68d
jz <jasontang02@outlook.com>
2023-10-17 17:51:06 +1100

    Remove dep google login

--- crm/src/pages/Login.jsx
+++ crm/src/pages/Login.jsx
@@ -9,7 +9,6 @@ import linkedinIcon from '../assets/linkedin.png';
 import avatars from "../assets/Avatar.png";
 import { primaryColor, secondaryColor }from '../utils/Color';
 import { toast, ToastContainer } from 'react-toastify';
-import { GoogleLogin } from 'react-google-login';
 import { gapi } from "gapi-script";
 import { useLinkedIn } from 'react-linkedin-login-oauth2';
 import linkedin from 'react-linkedin-login-oauth2/assets/linkedin.png';
@@ -383,7 +382,7 @@ function Login() {
           <hr className="separator-line" />
         </div>
         <OtherLoginOptions>
-          <GoogleLogin
+          {/* <GoogleLogin
             clientId="718066585548-9c25n26k4ci4do8f3mh45stub0s4de0u.apps.googleusercontent.com"
             buttonText="Login with Google"
             onSuccess={handleGoogleLoginSuccess}
@@ -393,7 +392,10 @@ function Login() {
                 <Icon src={googleIcon} alt="Google" onClick={renderProps.onClick}/>
               </IconOnlyButton>
             )}
-          />
+          /> */}
+          <IconOnlyButton>
+              <Icon src={googleIcon} alt="Google"/>
+            </IconOnlyButton>
           <IconOnlyButton>
             <Icon src={fbIcon} alt="Facebook" />
           </IconOnlyButton>
