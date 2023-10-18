305ac950cbb08ae89b579f212fcc2fce2f0e480b
jz <jasontang02@outlook.com>
2023-10-18 20:28:25 +1100

    Update APIRoutes.js
    change host to google cloud server

--- crm/src/utils/APIRoutes.js
+++ crm/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://backend-dot-voltaic-layout-402210.ts.r.appspot.com";
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
 export const receiveMessageRoute = `${host}/api/messages/getmsg`;
 
