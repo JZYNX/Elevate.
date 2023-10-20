9360887be5ae1642974bee7e806dc64d25af92f6
legityif <yifanyang80@gmail.com>
2023-10-20 14:04:44 +1100

    email styling

--- crm/src/components/Email.jsx
+++ crm/src/components/Email.jsx
@@ -46,7 +46,10 @@ const StyledForm = styled.form`
 `;
 
 const FormField = styled.div`
-  margin: 20px;
+  margin-left: 1.3rem;
+  margin-right: 1.3rem;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
 `;
 
 const Label = styled.label`
@@ -60,17 +63,24 @@ const Input = styled.input`
   border: 1px solid #ccc;
   border-radius: 5px;
   font-size: 16px;
+  &:focus {
+    border: 1px solid black;
+  }
 `;
 
 const Textarea = styled.textarea`
   width: 100%;
-  height: 150px;
+  height: 15rem;
   min-width: 458px;
   max-width: 458px;
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
   font-size: 16px;
+  &:focus {
+    border: 1px solid black;
+    outline: none;
+  }
 `;
 
 const SubmitButton = styled.button`
@@ -83,7 +93,9 @@ const SubmitButton = styled.button`
     font-size: 16px;
     transition: background-color 0.3s;
     width: 40%;
-    margin: auto;
+    margin-bottom: 1rem;
+    margin-left: auto;
+    margin-right: auto;
 
     &:hover {
     background-color: #0056b3;
